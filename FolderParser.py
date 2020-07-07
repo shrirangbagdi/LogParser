80179d8ad9cc99af74e62cc841d114e21f720942
shrirang bagdi <sbagdi2@illinois.edu>
2020-07-07 15:47:04 -0700

    Created file to include more functionality for parsing

---
+++ FolderParser.py
@@ -0,0 +1,27 @@
+import os
+
+from FileParser import FileParser
+
+
+class FolderParser:
+    def __init__(self, sourcePathway, destinationPathway, startDate=0, endDate=0, pattern=""):
+        self.sourcePathway = sourcePathway
+        self.destinationPathway = destinationPathway
+        self.startDate = startDate
+        self.endDate = endDate
+        self.pattern = pattern
+
+    def ParseFolder(self):
+        total_list = []
+        source = self.sourcePathway
+        destination = self.destinationPathway
+        for file in os.scandir(source):
+            if file.path.endswith(".log"):
+                total_list += FileParser(file, destination, self.startDate, self.endDate, self.pattern).ParseFile()
+        return total_list
+
+
+if __name__ == '__main__':
+
+    Parser = FolderParser('/Users/shrirangbagdi/Desktop/logs', '/Users/shrirangbagdi/Desktop/')
+    Parser.ParseFolder()
