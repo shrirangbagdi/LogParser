9431c10dc18c7afd518cfd3daf50cc822496213a
shrirang bagdi <sbagdi2@illinois.edu>
2020-07-24 16:45:33 -0700

    Updated to parse file for different patterns

--- FileParser.py
+++ FileParser.py
@@ -4,6 +4,10 @@ import re
 
 from datetime import datetime
 
+from PatternOne import PatternOne
+from PatternThree import PatternThree
+from PatternTwo import PatternTwo
+
 
 class FileParser:
     def __init__(self, logfile, destinationPathway, dateStart=0, dateEnd=0, pattern=""):
@@ -18,46 +22,105 @@ class FileParser:
         updated_list = self.UpdateWarningList(list_of_warnings)
         return updated_list
 
+    # { "name" : "khilawar", "File_Name": "unravel_us_1.log","Type": "INFO", "timestamp" :"2020-06-19 21:39:30.568"}
+
     def GenerateWarnings(self):
+
         list_of_warnings = []
         log_file = open(self.logfile, 'r')
         previous_type = ""
+        current_type = ""
         warning = {}
         first_iteration = True
-        first_phrase = 0
-        second_phrase = 1
-        third_phrase = 2
 
         for line in log_file:
-            if self.StartsCorrectly(line):
-                current_type = line.split()[third_phrase]
+            pattern_one = PatternOne(line)
+            pattern_three = PatternThree(line)
+            pattern_four = PatternFour(line)
 
-                if (not first_iteration) and ((previous_type == "WARN") or (previous_type == "ERROR")):
+            # check if warning is first iteration later......
+            if pattern_one.IsPatternOne():
+
+                if (not first_iteration) and (previous_type == "WARN" or previous_type == "ERROR"):
                     list_of_warnings.append(warning)
-                    warning = {}
+                    print(warning)
+                else:
+                    first_iteration = False
+
+                warning = {}
+                current_type = pattern_one.GetCurrentType()
 
                 if current_type == "WARN":
-                    line_list = line.split()
-                    date = line_list[first_phrase]
-                    time = line_list[second_phrase]
-                    message = line.split("WARN")[second_phrase]
-
-                    warning = {'File Name': self.GetLogFileName(), 'Date': date, 'Time': time, 'Type': current_type,
-                               'Message': message.strip()}
-                    first_iteration = False
+                    timestamp = pattern_one.GetTimeStamp()
+                    message = pattern_one.GetMessage()
+                    warning = {'File Name': self.GetLogFileName(), 'Timestamp': pattern_one.ConvertTimestamp(timestamp),
+                               'Type': current_type, 'Message': message.strip()}
 
                 if current_type == "ERROR":
-                    line_list = line.split()
-                    date = line_list[first_phrase]
-                    time = line_list[second_phrase]
-                    message = line.split("ERROR")[second_phrase]
-
-                    warning = {'File Name': self.GetLogFileName(), 'Date': date,
-                               'Time': time, 'Type': current_type,
-                               'Message': message}
-                    first_iteration = False
+                    timestamp = pattern_one.GetTimeStamp()
+                    message = pattern_one.GetMessage()
+                    warning = {'File Name': self.GetLogFileName(), 'Timestamp': pattern_one.ConvertTimestamp(timestamp),
+                               'Type': current_type, 'Message': message.strip()}
 
                 previous_type = current_type
+
+            elif pattern_three.IsPatternThree():
+
+                if (not first_iteration) and (previous_type == "WARN" or previous_type == "ERROR"):
+                    list_of_warnings.append(warning)
+                    print(warning)
+
+                else:
+                    first_iteration = False
+
+                warning = {}
+                current_type = pattern_three.GetCurrentType()
+
+                if current_type == "WARN":
+                    timestamp = pattern_three.GetTimeStamp()
+                    message = pattern_three.GetMessage()
+                    warning = {'File Name': self.GetLogFileName(),
+                               'Timestamp': pattern_three.ConvertTimestamp(timestamp),
+                               'Type': current_type, 'Message': message.strip()}
+
+                if current_type == "ERROR":
+                    timestamp = pattern_three.GetTimeStamp()
+                    message = pattern_three.GetMessage()
+                    warning = {'File Name': self.GetLogFileName(),
+                               'Timestamp': pattern_three.ConvertTimestamp(timestamp),
+                               'Type': current_type, 'Message': message.strip()}
+
+                previous_type = current_type
+
+
+            elif pattern_four.IsPatternFour():
+
+                if (not first_iteration) and (previous_type == "WARN" or previous_type == "ERROR"):
+                    list_of_warnings.append(warning)
+                    print(warning)
+
+                else:
+                    first_iteration = False
+
+                warning = {}
+                current_type = pattern_three.GetCurrentType()
+
+                if current_type == "WARN":
+                    timestamp = pattern_four.GetTimeStamp()
+                    message = pattern_four.GetMessage()
+                    warning = {'File Name': self.GetLogFileName(),
+                               'Timestamp': pattern_three.ConvertTimestamp(timestamp),
+                               'Type': current_type, 'Message': message.strip()}
+
+                if current_type == "ERROR":
+                    timestamp = pattern_four.GetTimeStamp()
+                    message = pattern_four.GetMessage()
+                    warning = {'File Name': self.GetLogFileName(),
+                               'Timestamp': pattern_three.ConvertTimestamp(timestamp),
+                               'Type': current_type, 'Message': message.strip()}
+
+                previous_type = current_type
+
 
             elif previous_type == "WARN" or previous_type == "ERROR":
                 warning["Message"] += line
@@ -95,16 +158,20 @@ class FileParser:
         end_date = datetime.strptime(self.dateEnd, '%Y/%m/%d')
 
         for dictionaries in listOfWarnings:
-            date_to_compare = datetime.strptime(dictionaries["Date"], '%Y/%m/%d')
+            date_to_compare = datetime.strptime(dictionaries["Date"], '%Y-%m-%d')
             if start_date < date_to_compare < end_date:
                 updated_list.append(dictionaries)
 
         return updated_list
 
+    # https://stackoverflow.com/questions/47896877/json-save-one-dict-per-line
     def CreateJsonFile(self, listOfWarnings):
         if listOfWarnings:
             with open(self.destinationPathway + self.GetJsonFileName() + '.json', 'w') as log_file:
-                json.dump(listOfWarnings, log_file, indent=4)
+                log_file.write('[' + ',\n'.join(json.dumps(i) for i in listOfWarnings) +
+                               ']\n')
+
+                # json.dump(listOfWarnings, log_file, indent=0)
 
     def GetJsonFileName(self):
         absolute_file_pathway = self.logfile
@@ -120,23 +187,8 @@ class FileParser:
 
         return file.split('/')[-1] + ext
 
-    def StartsCorrectly(self, line):
-        first_phrase = 0
-        second_phrase = 1
-        minimum_length = 24
-        minimum_spaces = 3
-        if (len(line) < minimum_length) or (len(line.split()) < minimum_spaces):
-            return False
-        else:
-            date = line.split()[first_phrase]
-            time = line.split()[second_phrase]
-
-            date_to_match = re.compile(r'\d\d\d\d/\d\d/\d\d')
-            time_to_match = re.compile(r'\d\d:\d\d:\d\d.\d\d\d')
-
-        return date_to_match.match(date) and time_to_match.match(time)
-
 
 if __name__ == '__main__':
     Parser = FileParser('/Users/shrirangbagdi/Desktop/f.log', "/Users/shrirangbagdi/Desktop/")
+    # Parser = FileParser('/home/ec2-user/logdata', "/Users/shrirangbagdi/Desktop/")
     Parser.ParseFile()
